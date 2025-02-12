0b7081abdd901037ea39a442f8a34838ba693720
Divyanshi <vishnoidivyanshi122@gmail.com>
2025-02-12 23:47:01 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center">
+      <h1>hey, this is divyanshi</h1>
+    </div>
+  );
+}
+
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+/** @type {import('vite').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
